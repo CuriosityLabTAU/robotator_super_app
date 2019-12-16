860ad9e9fed05bb96d40944a31ff4731e8265fc9
curious <torrpolakow@gmail.com>
2019-12-16 21:08:59 +0200

    added simple report

--- generate_report.py
+++ generate_report.py
@@ -1,6 +1,9 @@
 import rosbag
 from os import listdir
 from os.path import isfile, join
+import json
+import csv
+import numpy as np
 
 
 def parse_person(person_):
@@ -14,8 +17,6 @@ def parse_person(person_):
 
 def parse_data(data_):
     info = str(data_).split(' ')
-    for i in info:
-        print(i)
     id = [i for i, inf in enumerate(info) if 'PersonID' in inf] + [len(info)-1]
     data_ = {}
     for iid in range(0, len(id)-1):
@@ -24,25 +25,57 @@ def parse_data(data_):
 
 
 bag_files = [f for f in listdir('data/') if isfile(join('data/', f)) and '.bag' in f]
-print(bag_files)
+bag_files.sort()
 bag_filename = join('data/', bag_files[-1])
 print(bag_filename)
 bag = rosbag.Bag(bag_filename)
 data = {}
+total_speak = 0
 for topic, msg, t in bag.read_messages(topics=['/omer_data', '/omer_speaker_data']):
-    print(topic, msg)
-    # if topic == '/omer_data':
-    #     new_data = parse_data(msg)
-    #     for k, v in new_data.items():
-    #         if k not in data:
-    #             data[k] = {
-    #                 'Engagement': [],
-    #                 'Joy': [],
-    #                 'LookAtPerson': []
-    #             }
-    #         for i in data[k].keys():
-    #             data[k][i].append(v[i])
-    # if topic == '/ReSpeaker':
-    #     print(msg)
-    # print(data)
+    # print(topic, msg)
+    if topic == '/omer_data':
+        new_data = parse_data(msg)
+        for k, v in new_data.items():
+            if k not in data:
+                data[k] = {
+                    'Engagement': [],
+                    'Joy': [],
+                    'LookAtPerson': [],
+                    'Speak': []
+                }
+            for i in data[k].keys():
+                if i != 'Speak':
+                    data[k][i].append(v[i])
+    if topic == '/omer_speaker_data':
+        new_data = json.loads(msg.data)
+        id = 'PersonID:%d' % new_data['PersonID']
+        if id not in data:
+            data[id] = {
+                    'Engagement': [],
+                    'Joy': [],
+                    'LookAtPerson': [],
+                    'Speak': 0
+                }
+        data[id]['Speak'].append(new_data['time'])
+        total_speak += 1
 bag.close()
+
+report = []
+for k, v in data.items():
+    report.append({
+        'ID': k,
+        'Mean Joy': np.mean([float(j) for j in v['Joy']]),
+        'Mean Engagement': np.mean([float(e) for e in v['Engagement']]),
+        'Speak': float(len(v['Speak'])) * 100.0 / float(total_speak)
+    })
+print(report)
+csv_file = "report.csv"
+field_names = ['ID', 'Mean Joy', 'Mean Engagement', 'Speak']
+try:
+    with open(csv_file, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=field_names)
+        writer.writeheader()
+        for data in report:
+            writer.writerow(data)
+except IOError:
+    print("I/O error")
